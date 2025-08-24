8f3dd514a4f102614a86328b5afb025dd0ffa518
PrathmR <prathmeshredekar16@gmail.com>
2025-08-24 21:34:30 +0530

    pushing Simple_py codes

---
+++ Simple_py/list_empty.py
@@ -0,0 +1,7 @@
+def is_list_empty():
+    input_list = input("Enter the list elements: ")
+    if not input_list:
+        print("The list is empty.")
+    else:
+        print("The list is not empty.")
+is_list_empty()
